80f0ef634cdbe304ce7f20d129ba59a27b547ca7
dimrisaurabh <dimrisaurabh@gmail.com>
2018-04-16 21:33:57 +0530

    Commiting a piece of code with blanks to be expand

---
+++ hi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void sum();	//Expand this piece of code 
+void multiply();
+int main()
+	
+{
+	int a,b;
+	cout<<"Enter the values of a and b";
+	cin>>a>>b;
+	sum();
+	multiply();
+	return 0;
+}
