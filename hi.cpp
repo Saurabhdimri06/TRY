3ef5a47ad4cbab5622840d6372ac121b7152ff8b
dimrisaurabh <dimrisaurabh@gmail.com>
2018-04-16 21:39:09 +0530

    CHanges done in program

--- hi.cpp
+++ hi.cpp
@@ -1,14 +1,31 @@
 #include<iostream>
 using namespace std;
-void sum();	//Expand this piece of code 
-void multiply();
+void sum(a,b);	//Expand this piece of code 
+void multiply(a,b);
+void divide(a,b);
+{
+	float c;
+	c = a/b;
+	cout<<c;
+}
 int main()
 	
 {
-	int a,b;
+	int a,b,ch;
 	cout<<"Enter the values of a and b";
 	cin>>a>>b;
-	sum();
-	multiply();
+	cout<<"Enter the choice \n1) SUM\n2) MULTIPLY\n3) DIVIDE";
+	cin>>ch;
+	switch(ch)
+	{
+		case 1: 	sum(a,b);
+				break;
+		case 2:		multiply(a,b);
+				break;
+		case 3:		divide(a,b);
+				break;
+		default : cout<<"WRONG CHOICE!!!"
+			  	break;
+	}
 	return 0;
 }
